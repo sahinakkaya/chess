a7c0e5b76162f773fcfdf86e827a34ce6745c60c
Asocia <akkayas17@itu.edu.tr>
2019-12-10 12:12:51 +0300

    Convert all ui files if nothing is given

--- chessVshogi/UI/convert_to_py.sh
+++ chessVshogi/UI/convert_to_py.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
+if [ "$#" -ne 1 ]; then
+  c=0
+  for f in *.ui
+  do
+    ui_file=$(basename -- "$f")
+    python_file="${ui_file%.*}.py"
+    pyuic5 ${ui_file} -o ${python_file}
+    ((c+=1))
+  done
+  echo "Converted $c files succesfully"
+else
+  ui_file=$(basename -- "$1")
+  python_file="${ui_file%.*}.py"
 
-ui_file=$(basename -- "$1")
-python_file="${ui_file%.*}.py"
-
-pyuic5 ${ui_file} -o ${python_file}
+  pyuic5 ${ui_file} -o ${python_file}
+  echo "Converted ${ui_file} to a python file succesfully"
+fi
