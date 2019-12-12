716665daab418b8914853b7218711f043bdfa81f
CIO61 <enst61@gmail.com>
2019-12-13 00:46:51 +0300

    Add dynamic piece graphic generation to ingame_shogi.py

--- chessVshogi/UI/ingame_ui.py
+++ chessVshogi/UI/ingame_ui.py
@@ -1,6 +1,12 @@
-from ingame_chess import *
+tester = False
+if tester:
+    from ingame_chess import *
+    ui_item = Ui_Form
+else:
+    from ingame_shogi import *
+    ui_item = Ui_Ingame_screen
 
-Chess_Piece_Resource_Corresp = {
+Piece_Resource_Corresp = {
     "C_WP":":/Chess/resources/chess_48/Chess_plt48.png",
     "C_WR":":/Chess/resources/chess_48/Chess_rlt48.png",
     "C_WN":":/Chess/resources/chess_48/Chess_nlt48.png",
@@ -12,22 +18,38 @@ Chess_Piece_Resource_Corresp = {
     "C_BN":":/Chess/resources/chess_48/Chess_ndt48.png",
     "C_BB":":/Chess/resources/chess_48/Chess_bdt48.png",
     "C_BQ":":/Chess/resources/chess_48/Chess_qdt48.png",
-    "C_BK":":/Chess/resources/chess_48/Chess_kdt48.png"
+    "C_BK":":/Chess/resources/chess_48/Chess_kdt48.png",
+    "S_BP":":/Ichiji/resources/Ichiji/Gfu.png",
+    "S_BL":":/Ichiji/resources/Ichiji/Gkyo.png",
+    "S_BN":":/Ichiji/resources/Ichiji/Gkei.png",
+    "S_BS":":/Ichiji/resources/Ichiji/Ggin.png",
+    "S_BG":":/Ichiji/resources/Ichiji/Gkin.png",
+    "S_BK":":/Ichiji/resources/Ichiji/Ggyoku.png",
+    "S_BB":":/Ichiji/resources/Ichiji/Gkaku.png",
+    "S_BR":":/Ichiji/resources/Ichiji/Ghi.png",
+    "S_WP":":/Ichiji/resources/Ichiji/Sfu.png",
+    "S_WL":":/Ichiji/resources/Ichiji/Skyo.png",
+    "S_WN":":/Ichiji/resources/Ichiji/Skei.png",
+    "S_WS":":/Ichiji/resources/Ichiji/Sgin.png",
+    "S_WG":":/Ichiji/resources/Ichiji/Skin.png",
+    "S_WK":":/Ichiji/resources/Ichiji/Sou.png",
+    "S_WB":":/Ichiji/resources/Ichiji/Skaku.png",
+    "S_WR":":/Ichiji/resources/Ichiji/Shi.png",
 }
 
-class Window(QtWidgets.QWidget, Ui_Form):
+class Window(QtWidgets.QWidget, ui_item):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.show()
         self.tiles = []
-        for i in range(11, 89):
+        for i in range(11, 100):
             try:
                 tile = getattr(self, "Tile_{}".format(i))
                 self.tiles.append(tile)
                 tile.installEventFilter(self)
                 if tile.property("Piece") != '':
-                    tile.setPixmap(QtGui.QPixmap(Chess_Piece_Resource_Corresp[tile.property("Piece")]))
+                    tile.setPixmap(QtGui.QPixmap(Piece_Resource_Corresp[tile.property("Piece")]))
             except AttributeError:
                 pass
 
