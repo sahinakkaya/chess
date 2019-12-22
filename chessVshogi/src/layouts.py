71157de648586f108b1a2105dc58aecde5532d7b
CIO61 <enst61@gmail.com>
2019-12-22 23:03:27 +0300

    Revert testing pawn from chess_default

--- chessVshogi/src/layouts.py
+++ chessVshogi/src/layouts.py
@@ -12,12 +12,12 @@ hybrid_default = [
 
 chess_default = [
     ['C_WR', 'C_WN', 'C_WB', 'C_WQ', 'C_WK', 'C_WB', 'C_WN', 'C_WR'],
-    ['C_WP', 'C_WP', 'C_WP', 'C_WP', 'C_WP', 'C_WP', 'C_WP', ''],
+    ['C_WP', 'C_WP', 'C_WP', 'C_WP', 'C_WP', 'C_WP', 'C_WP', 'C_WP'],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
-    ['C_BP', 'C_BP', 'C_BP', 'C_BP', 'C_BP', 'C_BP', 'C_BP', 'C_WP'],
+    ['C_BP', 'C_BP', 'C_BP', 'C_BP', 'C_BP', 'C_BP', 'C_BP', 'C_BP'],
     ['C_BR', 'C_BN', 'C_BB', 'C_BQ', 'C_BK', 'C_BB', 'C_BN', 'C_BR']
     ]
 
